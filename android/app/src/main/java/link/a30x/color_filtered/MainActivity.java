55e5d7ca67a2262d29365bb7817a0e3393008ffc
hekaijun <hekaijun@xiaoheiban.cn>
2020-10-10 17:32:33 +0800

    init commit

---
+++ android/app/src/main/java/link/a30x/color_filtered/MainActivity.java
@@ -0,0 +1,6 @@
+package link.a30x.color_filtered;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'ColorFiltered'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: <Widget>[
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            child: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Colors.blue,
+                BlendMode.color,
+              ),
+              child: Image.asset('assets/source.png'),
+            ),
+          ),
+          Image.asset('assets/source.png'),
+        ],
+      ),
+    );
+  }
+}
